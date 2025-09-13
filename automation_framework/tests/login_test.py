a72e95a655bcf7b06ce30e5d5268e071e3f15517
qdoan96 <lequyendoan1005@gmail.com>
2025-09-13 20:55:11 +0700

    addagain

--- automation_framework/tests/login_test.py
+++ automation_framework/tests/login_test.py
@@ -1,6 +1,6 @@
 
 from automation_framework.base.base_test import BaseTest
-from automation_framework.pages.login_page import LoginPage
+from automation_framework.pages.login_page_new import LoginPage
 from automation_framework.pages.dashboard_page import DashboardPage
 from automation_framework.pages.recruitment_page import RecruitmentPage
 from time import sleep
@@ -9,9 +9,9 @@ import pytest
 class TestLogin(BaseTest):
     def test_login(self):
         login_page = LoginPage(self.driver) # khoi tao login_page
-        login_page.login() # goi ham login
-        sleep (3)
+        login_page.login('Admin', 'admin123') # goi ham login tu login_page
         dashboard_page = DashboardPage(self.driver) # khoi tao dashboard_page
         dashboard_page.is_dashboard_displayed()
+        
 
     
